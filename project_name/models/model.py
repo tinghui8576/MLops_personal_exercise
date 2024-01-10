eb1f311ed804d6374ef88befbcfaacbfca4f8a7a
Ting-Hui Cheng鄭婷卉 <tinghui1738cheng@gmail.com>
2024-01-11 00:27:26 +0100

    T5 models

--- project_name/models/model.py
+++ project_name/models/model.py
@@ -1,26 +1,51 @@
 import torch
 
-class MyNeuralNet(torch.nn.Module):
-    """ Basic neural network class. 
-    
-    Args:
-        in_features: number of input features
-        out_features: number of output features
-    
-    """
-    def __init__(self, in_features: int, out_features: int) -> None:
-        self.l1 = torch.nn.Linear(in_features, 500)
-        self.l2 = torch.nn.Linear(500, out_features)
-        self.r = torch.nn.ReLU()
-    
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass of the model.
+from transformers import T5ForConditionalGeneration, AdamW, T5Tokenizer
+
+
+class Model(torch.nn.Module):
+    def __init__(self, lr=5e-5):
+        super().__init__()
         
-        Args:
-            x: input tensor expected to be of shape [N,in_features]
+        self.model = T5ForConditionalGeneration.from_pretrained("t5-small")
+        self.tokenizer = T5Tokenizer.from_pretrained("t5-base")
+        self.lr = lr
 
-        Returns:
-            Output tensor with shape [N,out_features]
+    def forward(self, input_ids, attention_mask, labels=None):     
+        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+        return outputs
+    
+    def generate(self, input_ids):
+        # Use the generate method from the T5 model
+        generated_ids = self.model.generate(input_ids)
+        generated_text = self.tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+        return generated_text
+    
+    def common_step(self, batch, batch_idx):
+        outputs = self(**batch)
+        loss = outputs.loss
 
-        """
-        return self.l2(self.r(self.l1(x)))
+        return loss
+    
+    def training_step(self, batch, batch_idx):
+        loss = self.common_step(batch, batch_idx)     
+        # logs metrics for each training_step,
+        # and the average across the epoch
+        self.log("training_loss", loss)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.common_step(batch, batch_idx)     
+        self.log("validation_loss", loss, on_epoch=True)
+
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        loss = self.common_step(batch, batch_idx)     
+
+        return loss
+
+    def configure_optimizers(self):
+        
+        return AdamW(self.parameters(), lr=self.lr)
